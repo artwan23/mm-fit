e333ad713feb9cad69084c24ad887cd15f2cb748
artwan23 <125972432+artwan23@users.noreply.github.com>
2024-07-07 19:40:19 +0200

    Create ExDataset_v2.py

---
+++ ExDataset_v2.py
@@ -0,0 +1,85 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.animation import FuncAnimation, writers    
+    
+    
+def plot_3d_pose(ax, pose):
+    """
+    Visualize a 3D skeleton.
+    :param pose: numpy array (3 x 17) with x, y, z coordinates with COCO keypoint format.
+    :param elev: Elevation angle in the z plane.
+    :param azim: Azimuth angle in the x, y plane.
+    :param figsize: Figure size.
+    :return: None
+    """
+    pose = pose.flatten(order='F')
+    vals = np.reshape(pose, (17, -1))
+    artists = []
+
+    #ax.view_init(elev, azim)
+    
+    #fig = plt.figure(figsize=figsize)
+    #ax = Axes3D(fig)
+    #ax.view_init(elev, azim)
+    
+    
+    limbs = [(0, 1), (1, 2), (2, 3), (0, 4), (4, 5), (5, 6), (0, 7), (7, 8), (8, 9), (9, 10), (8, 11), (11, 12),
+             (12, 13), (8, 14), (14, 15), (15, 16)]
+    left_right_limb = np.array([1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1])
+    for i, limb in enumerate(limbs):
+        x, y, z = [np.array([vals[limb[0], j], vals[limb[1], j]]) for j in range(3)]
+        if left_right_limb[i] == 0:
+            cc = 'blue'
+        elif left_right_limb[i] == 1:
+            cc = 'red'
+        else:
+            cc = 'black'
+        lines = ax.plot(x, y, z, marker='o', markersize=2, lw=1, c=cc)
+        artists.extend(lines)
+        
+    return artists
+
+
+loaded_data = np.load('/home/adam/Downloads/mm-fit/w00/w00_pose_3d.npy')
+
+fig = plt.figure(figsize=(8,8))
+ax = fig.add_subplot(111,projection='3d')
+f= loaded_data.shape[1] - 60000
+ims =[]
+
+for frame in range(f):
+    data = loaded_data[:,frame,1:]
+    sf = plot_3d_pose(ax, pose=data)
+    ims.append(sf)
+    
+#skel_3d = loaded_data[:, 5, :]
+#skel_3d.shape
+
+#data = loaded_data[:,frame,:]
+#plot_3d_pose(ax, pose=loaded_data[:,1,1:])
+
+def animate(i):
+    ax.clear()
+    ax.set_xlim((-500, 500))
+    ax.set_ylim((-500, 500))
+    ax.set_zlim((-500, 1500))
+    for line in ims[i]:
+        ax.add_line(line)
+        img=line
+    return ims[i]
+
+anim= FuncAnimation(fig, animate, frames=f, interval=10)
+
+#Writer = writers['ffmpeg']
+#writer = Writer(fps=10, metadata=dict(artist='Me'), bitrate=1800)
+#anim.save('3d_animation.mp4', writer=writer)
+
+a = animate(10)
+plt.show()
+
+
+
+
+
